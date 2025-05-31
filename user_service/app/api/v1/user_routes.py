9032272b6afdbb280507cf7049750e9375745a37
bnmbatha <bnmbatha@amazon.com>
2025-05-31 23:57:48 +0200

    Update user_routes.py

--- user_service/app/api/v1/user_routes.py
+++ user_service/app/api/v1/user_routes.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from app.db.session import SessionLocal
 from app.schemas.user import UserCreate, UserOut
 from app.services.user_service import create_user
+from fastapi import HTTPException
 
 router = APIRouter()
 
@@ -16,3 +17,10 @@ def get_db():
 @router.post("/register", response_model=UserOut)
 def register(user: UserCreate, db: Session = Depends(get_db)):
     return create_user(db, user)
+
+@router.get("/{user_id}", response_model=UserOut)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
