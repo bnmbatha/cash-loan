6f3784329a789f84b79317d2c28afb3c7a5beeaf
bnmbatha <bnmbatha@amazon.com>
2025-06-01 20:20:41 +0200

    Update config.py

--- loan_service/app/core/config.py
+++ loan_service/app/core/config.py
@@ -5,7 +5,10 @@ class Settings(BaseSettings):
     DB_USER: str
     DB_PASSWORD: str
     DB_NAME: str
+    DB_PORT: int = 5432
     SECRET_KEY: str
+    aws_region: str
+    aws_secret_name: str
 
     class Config:
         env_file = ".env"
