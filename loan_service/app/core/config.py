5820777223a7c2a5db68dd3a906be57e03905bb9
bnmbatha <bnmbatha@amazon.com>
2025-06-01 20:12:59 +0200

    Update config.py

---
+++ loan_service/app/core/config.py
@@ -0,0 +1,13 @@
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_NAME: str
+    SECRET_KEY: str
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
