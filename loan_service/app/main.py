928bf3c3c60d263e7139999b1c3f62153e0df8bc
bnmbatha <bnmbatha@amazon.com>
2025-06-02 16:48:29 +0200

    Update main.py

--- loan_service/app/main.py
+++ loan_service/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.api.v1.endpoints import loan
+from app.api.v1.endpoints import approval
 
 app = FastAPI(title="Loan Service")
 
 app.include_router(loan.router, prefix="/api/v1/loans", tags=["loans"])
+app.include_router(approval.router, prefix="/api/v1/loans", tags=["approval"])
