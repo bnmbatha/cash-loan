2e4a62f202ac5c6f88c9f2b17ab67c9a588de03d
bnmbatha <bnmbatha@amazon.com>
2025-06-03 09:10:52 +0200

    Update create_db.py

--- user_service/create_db.py
+++ user_service/create_db.py
@@ -1,6 +1,16 @@
+# Import the SQLAlchemy engine instance used to connect to the database
 from app.db.session import engine
+
+# Import the base class that holds metadata about all database models
 from app.db.models.user import Base
 
+# Log that the table creation process has started
 print("Creating tables...")
+
+# This line uses SQLAlchemy's metadata to create all tables defined under Base
+# It checks the metadata and creates tables in the DB if they don't already exist
 Base.metadata.create_all(bind=engine)
+
+# Log that the process is finished
 print("Done.")
+
