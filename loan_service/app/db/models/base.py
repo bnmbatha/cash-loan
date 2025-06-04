5d78d0a02ed5f31d64157ac513d974fae834e107
bnmbatha <bnmbatha@amazon.com>
2025-06-05 00:19:55 +0200

    Create base.py

---
+++ loan_service/app/db/models/base.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String
+from app.db.models.base import Base  # or BaseModel if you're using the mixin
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
