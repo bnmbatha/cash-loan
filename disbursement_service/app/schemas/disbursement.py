0ea5127752f9159276eef90de32fb2461e260b96
bnmbatha <bnmbatha@amazon.com>
2025-06-05 18:59:16 +0200

    Update disbursement.py

---
+++ disbursement_service/app/schemas/disbursement.py
@@ -0,0 +1,19 @@
+# app/schemas/disbursement.py
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
+
+class DisbursementCreate(BaseModel):
+    loan_id: int
+    user_id: int
+    amount: float
+
+class DisbursementOut(DisbursementCreate):
+    id: int
+    status: str
+    reference: Optional[str]
+    created_at: datetime
+    completed_at: Optional[datetime]
+
+    class Config:
+        orm_mode = True
